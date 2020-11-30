4f7abfb1b8661f871f32dd24acfdc4f670f956ea
Abreu-Ricardo <61216670+Abreu-Ricardo@users.noreply.github.com>
2020-11-30 10:52:34 -0400

    Update cli.py

--- cli.py
+++ cli.py
@@ -2,74 +2,62 @@ import socket
 import sys
 
 # 65 MB
-HEADER = 1024*65 #51200 # 50 MB
-PORT = 8080
+HEADER = 1024*65 
+PORTA = 8080
 
-SERVER = socket.gethostbyname(socket.getfqdn()) #"127.0.1.1"
-ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
+# Inicia um server localhost com o parametro vazio
+SERVER = socket.gethostbyname(socket.getfqdn())
+
+ADDR = (SERVER, PORTA)
+CODIFICACAO = 'utf-8'
+MSG_SAIDA = "DESCONECTAR!"
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
 
 def listagem(acao):
-    acao.encode(FORMAT)
-    client.send(bytes( acao, FORMAT))
+    acao.encode(CODIFICACAO)
+    client.send(bytes( acao, CODIFICACAO))
 
-    msg = client.recv(HEADER).decode(FORMAT)
+    msg = client.recv(HEADER).decode(CODIFICACAO)
     print(msg)
 
 def envio(acao):
-    acao.encode(FORMAT)
-    client.send(bytes(acao, FORMAT))
+    # Envia acao
+    acao.encode(CODIFICACAO)
+    client.send(bytes(acao, CODIFICACAO))
 
     #Digitar o nome 
     print("Digite o nome:")
     nome = input()
-
     nome.encode()
-    client.send(bytes( nome, FORMAT))
+    client.send(bytes( nome, CODIFICACAO))
 
-
-    #if acao == DISCONNECT_MESSAGE:
-    print(client.recv(HEADER).decode(FORMAT))
+    print(client.recv(HEADER).decode(CODIFICACAO))
     print()
 
 
 def saida(msg):
     msg.encode("utf-8")
     client.send(bytes(msg, "utf-8"))
-    print(client.recv(HEADER).decode(FORMAT))
-
-# def send(msg):
-#     message = msg.encode(FORMAT)
-#     msg_length = len(message)
-#     send_length = str(msg_length).encode(FORMAT)
-#     send_length += b' ' * (HEADER - len(send_length))
-
-#     client.send(send_length)
-#     client.send(message)
-#     if msg == DISCONNECT_MESSAGE:
-#         print(client.recv(2048).decode(FORMAT))
+    print(client.recv(HEADER).decode(CODIFICACAO))
 
 
 def sendFile(acao):
-    acao.encode(FORMAT)
-    client.send(bytes( acao, FORMAT))
+    acao.encode(CODIFICACAO)
+    client.send(bytes( acao, CODIFICACAO))
     
     print("Nome do arquivo")
     nome = input()
-    nome.encode(FORMAT)
-    client.send(bytes(nome, FORMAT))
+    nome.encode(CODIFICACAO)
+    client.send(bytes(nome, CODIFICACAO))
 
     file = open("entrada.txt", "r")
     newFile = file.read()
-    newFile.encode(FORMAT)
+    newFile.encode(CODIFICACAO)
+    client.send(bytes(newFile, CODIFICACAO))
 
-    client.send(bytes(newFile, FORMAT))
-
-    msgServer = client.recv(HEADER).decode(FORMAT)
+    msgServer = client.recv(HEADER).decode(CODIFICACAO)
     print(msgServer)
 
    
@@ -91,33 +79,22 @@ while msg != 0:
     msg = int(acao)
     
     if msg == 1:
-        #print("Digite o nome do arquivo:")
         sendFile(acao)
-        #send(acao, nome)
 
     elif msg == 2:
-        #print("Digite o nome do diretorio:")
-        #nome = input()
-        #arq = os.getsize(nome)
         envio(acao)
 
     elif msg == 3:
-        #print("Digite o nome do diretorio o qual quer listar")
-        #acao = input()
         listagem(acao)
 
     elif msg == 4:
-        #print("Digite o nome do diretorio:")
-        #acao = input()
         envio(acao)
     
     elif msg == 5:
-        #print("Digite o nome do diretorio:")
-        #acao = input()
         envio(acao)
 
     elif msg == 0:
-        saida(DISCONNECT_MESSAGE)
+        saida(MSG_SAIDA)
         
     
 
